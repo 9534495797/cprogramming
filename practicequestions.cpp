59cf2c08c3b7d9efcb04eb3ff733b7d3c53e482b
9534495797 <chaubeyankit927@gmail.com>
2023-02-14 19:30:30 +0530

    ok

---
+++ practicequestions.cpp
@@ -0,0 +1,79 @@
+#include<stdio.h>
+//int main(){
+//	int a;
+//	scanf("%d",&a);
+//	if (a%2==0){
+//		printf("even");
+//		
+//	}
+//	else{
+//		printf("odd");
+//	}
+//	
+//}
+
+// questio2
+//int main(){
+//	int a;
+//	scanf("%d",&a);
+//	if (a%2==0 && a%4==0){
+//		printf("divide by 2 and 4");
+//	}
+//	else{
+//		printf("not divide by both");
+//	}
+//}
+// question3
+//int main(){
+//	int a,b,c;
+//	scanf("%d",&a);
+//	scanf("%d",&b);
+//	scanf("%d",&c);
+//	if (a>b && a>c){
+//		printf("a is largest");
+//	}
+//	else if(b>a && b>c){
+//		printf("b is largest");
+//	}
+//	else{
+//		printf("c is largest");
+//		
+//	}
+//	
+//}
+
+// question 4 done by condiotional operator method
+//int main(){
+//	int a;
+//	printf("value of a ");
+//	scanf("%d",&a);
+//	a%2==0?printf("even"):printf("odd");
+//}
+
+//int main(){
+//	int a,b,c;
+//	scanf("%d",&a);
+//	scanf("%d",&b);
+//	switch(a,b){
+//		case '+'
+//	}
+//}
+
+//question 6
+//int main(){
+//	int a,b,c;
+//	scanf("%d",&a);
+//	scanf("%d",&b);
+//	scanf("%d",&c);
+//	if (a>b && a>c){
+//		printf("a is second largest %d",a);
+//	}
+//	else if(b>a && b<c){
+//		printf("b is second largest %d",b);
+//	}
+//	else{
+//		printf("c is second largest %d",c);
+//	}
+//}
+
+
