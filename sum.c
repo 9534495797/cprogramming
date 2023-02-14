80491527d6d069cfc4e6dbb4c3a98eeab4907b13
9534495797 <chaubeyankit927@gmail.com>
2023-02-14 19:27:08 +0530

    ok

---
+++ sum.c
@@ -0,0 +1,134 @@
+
+//#include<stdio.h>
+//void main (){    //can use void instead of int
+//	int a= 6;
+//	int b=5;
+//	int c=a*b;
+//	printf("%d",c);
+//}
+
+
+//#include<stdio.h>
+//void main(){       //can use void instead of float
+//	float a=2.5;
+//	float b=5.0;
+//	float c=a/b;
+//	printf("%f",c);  // f stands for format
+//}
+
+//toke is smallest unit in program
+// token are of seven type strings,keyword,identifiers,constant,variable,operators and special char
+
+// special constant /n means new line,/t single space 
+//variables can not start with number no special char and no space in it
+//variable start with alphabets and underscore
+//it is case sensitive means mentain lower and upper case
+//upper case
+//name give
+
+
+
+// data types integer,float,character,double is same like float but number of digit more
+// for integer %d, for float %d,for char %c,double %f
+
+
+
+
+//#include<stdio.h>
+//void main (){
+//	int a=4;
+//	float pi=3.14;
+//	printf("%f",2*pi*a);
+//}
+
+//#include<stdio.h>
+//void main(){
+//	int a=15;
+//	float pi=3.14;
+//	int p=2*a*pi;
+//	printf("%d",p);
+//
+//		
+//	}
+
+
+
+
+//#include<stdio.h>
+//void main(){
+//	int a=15;
+//	float pi=3.14;
+//	int p=2*a*pi;
+//	printf("%f",p);
+//}
+
+//#include<stdio.h>
+//void main(){
+//	int a=15;
+//	float pi=3.14;
+//	int b=(int)pi;
+//	printf("%d",2*b*a);
+//}
+
+// 07/02/23
+// we use scanf to take user input in c
+//#include<stdio.h>
+//void main(){
+//	int a,b;
+//	printf("enter number \n");
+//	scanf("%d",&a);
+//	printf("enter number \n");
+//	scanf("%d",&b);
+//	printf("%d",a+b);
+//	printf("%d",a-b);
+//	printf("%d",a*b);
+//	printf("%d",a+b);
+//	
+//	
+//}
+//
+
+//#include<stdio.h>
+//void main(){
+//	int a=9,b=2;
+//	printf("%d",a%b);
+//	int a1=-9,b1=2;
+//	printf("%d",a1%b1);
+//	int a2=9,b2=-2;
+//	printf("%d",a2%b2);
+//	int a3=-9,b3=-2;
+//	printf("%d",a3%b3);
+//	
+//}
+
+
+//#include<stdio.h>
+//void main(){
+//	int a=15;
+//	int c=a++;
+//	
+//	printf("%d,%d",c,a);
+//}
+
+//#include<stdio.h>
+//void main(){
+//	int a=15;
+//	int c=a++ + a++;
+//	printf("%d,%d",c,a);
+//}
+
+//#include<stdio.h>
+//void main(){
+//	int m=10;
+//	int n,n1;
+//	n=--m;
+//	n1=m--;
+//	n-=n1;
+//	printf("%d",n);
+//}
+
+
+
+
+
+
