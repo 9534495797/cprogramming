a83c68647df8df39bfd96d756074679a84ee3bd9
9534495797 <chaubeyankit927@gmail.com>
2023-02-16 19:00:01 +0530

    ok

---
+++ forloop.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//int main(){
+//	int n;
+//	for (n=10;n>0;n=n-1){
+//	printf("%d",n);
+//	}
+//}
+
+
+//int main(){
+//	int n;
+//	for(n=1;n<=20;n=n+2){
+//		printf("%d",n);
+//			}
+//}
+int main(){
+	int n;
+	for(n=2;n<=20;n=n+2){
+		printf("%d",n);
+	}
+}
