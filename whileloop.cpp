c4be9cb8cc6b1cbfd6d3a8e41482f57c2a97660a
9534495797 <chaubeyankit927@gmail.com>
2023-02-16 18:58:40 +0530

    ok

---
+++ whileloop.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int n=10;
+	while (n>0){
+		n=n-1;
+		printf("%d\n",n);
+		//n=n-1;
+	}
+}
+
+
+//#include<stdio.h>
+//int main(){
+//	int a=3,j=1;
+//	
+//	while (j<=12){
+//		
+//		printf("%d * %d =%d \n",a,j,a*j);
+//		j=j+1;
+//	}
+//	
+//	
+//}
+
+
+
