c566b6736af59d5df5a8776d22e2091592020b87
9534495797 <chaubeyankit927@gmail.com>
2023-02-17 21:37:39 +0530

    ok

---
+++ sumpractice.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	printf("your output=");
+	int c=x+y;
+	int d=x-y;
+	int e=x*y;
+	int f=x/y;
+	printf("%d\n",c);
+	printf("%d\n",d);
+	printf("%d\n",e);
+	printf("%d\n",f);
+}
