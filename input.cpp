2ce6e9fab28472a356e870d7d31a15430abac1c9
9534495797 <chaubeyankit927@gmail.com>
2023-02-17 21:38:31 +0530

    ok

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//int main(){
+//	int x;
+//	printf("your input");
+//	scanf("%d",&x);
+//	printf("%d",x);
+//}
+
+
+int main(){
+	int x,y;
+	printf("your input=");
+	printf("next input=");
+	scanf("%d",&x);
+	scanf("%d",&y);
+	printf("%d",x);
+	printf("%d",y);
+}
