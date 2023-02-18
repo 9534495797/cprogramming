7c6b73af34d3c4179743227e2d094688ee2932ad
9534495797 <chaubeyankit927@gmail.com>
2023-02-18 21:32:04 +0530

    ok

---
+++ forlooppractice.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//int main(){
+//	int i=0;
+//	for(i=1;i<=4;i++){
+//		printf("%d\n",i);
+//	}
+//}
+
+
+int main(){
+	int i=0;
+	for(i=2;i<=100;i=i+2){
+		printf("%d\n",i);
+	}
+}
