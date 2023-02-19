707d895cbbaa06663aec2ed442a7a09e8ac68506
9534495797 <chaubeyankit927@gmail.com>
2023-02-19 20:08:33 +0530

    ok

---
+++ whilelooppractice.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//int main(){
+//	int i;
+//	while(i<20){
+//		i=i+2;
+//		printf("%d\n",i);
+//	}
+//}
+
+int main(){
+	int i,j;
+	scanf("%d",&i);
+	scanf("%d",&j);
+	while(j<=15){
+		printf("%d %d %d",i,j,i+j);
+	}
+}
