54b01fee7e3616b9a6afdffe72a60a3200bcb4e4
9534495797 <chaubeyankit927@gmail.com>
2023-02-19 19:53:11 +0530

    ok'

---
+++ ifelsepractice.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//int main(){
+//	int x,y;
+//	printf("you input x=");
+//	scanf("%d",&x);
+//	printf("your input y=");
+//	scanf("%d",&y);
+//	if (x>y){
+//		printf("x is greater than y",x);
+//	}
+//	else{
+//		printf("x is less than y",y);
+//	}
+//}
+
+//int main(){
+//	int x,y,z;
+//	scanf("%d",&x);
+//	scanf("%d",&y);
+//	scanf("%d",&z);
+//	if (x>y && x>z){
+//		printf("x is largest");
+//	}
+//	else if(y>x && y>z){
+//		printf("y is largest");
+//	}
+//	else{
+//		printf("z is largest");
+//	}
+//}
