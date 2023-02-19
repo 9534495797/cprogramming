33293d3a995fc23abdb7e7b1ca500c855572ef17
9534495797 <chaubeyankit927@gmail.com>
2023-02-19 19:38:11 +0530

    ok

---
+++ for_looppractice.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+//int main(){
+//	int i=0;
+//	for (i=1;i<=100;i++){
+//		printf("%d\n",i);
+//	}
+//}
+
+
+//int main(){
+//	int i;
+//	printf("your input=");
+//	scanf("%d",&i);
+//	
+//	for (i;i<=20;i=i+2){
+//		printf("%d\n",i);
+//	}
+//}
+
+//int main(){
+//	int i=0;
+//	for (i=0;i<=100;i=i+2){
+//		printf("%d\n",i);
+//	}
+//}
