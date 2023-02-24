24a6a561be4bcc6e0f8dfd64d63d614ac16320be
9534495797 <chaubeyankit927@gmail.com>
2023-02-24 16:31:46 +0530

    ok

---
+++ dowhileloop.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//int main(){
+//	int n=10;
+//	do{
+//		printf("%d\n",n);
+//		n=n-1;
+//	}while (n>0);
+//}
+
+
+int main(){
+	int n=10;
+	do{
+		printf("%d",n);
+		n=n-1;
+		
+	}while(n<0);
+}
