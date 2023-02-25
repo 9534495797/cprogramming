d871ea320c210eddb5824c849b9512b178fb9b95
9534495797 <chaubeyankit927@gmail.com>
2023-02-25 20:14:55 +0530

    'ok

---
+++ reversepractice.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	printf("%d",reverse());
+}
+int reverse(){
+	int a,d,rev=0;
+	printf("enter the value");
+	scanf("%d",&a);
+	while(a!=0){
+		d=a%10;
+		a=a/10;
+		rev=rev*10+d;
+	}
+	return rev;
+}
