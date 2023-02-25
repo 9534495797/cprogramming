f75f12cbe3ec6dd4ec3cf330903acc3b6fa77e37
9534495797 <chaubeyankit927@gmail.com>
2023-02-25 20:13:27 +0530

    ok

---
+++ reverse45.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int reverse();
+int main(){
+	printf("%d",reverse());
+	return 0;
+}
+int reverse(){
+	int a,d,rev=0;
+	printf("enter the value");
+	scanf("%d",&a);
+	while(a!=0){
+		d=a%10;
+		a=a/10;
+		rev=rev*10+d;
+	}
+	return rev;
+}
