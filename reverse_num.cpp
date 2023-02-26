3a0c5581bc79c329c6f6262a37bb82aedcfc5f2f
9534495797 <chaubeyankit927@gmail.com>
2023-02-26 14:37:52 +0530

    ok

---
+++ reverse_num.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	printf("%d",reverse());
+}
+int reverse(){
+	int a,d,rev;
+	printf("enter the value");
+	scanf("%d",&a);
+	while(a!=0){
+		d=a%10;
+		a=a/10;
+		rev=rev*10+d;
+	}
+	return rev;
+}
+
