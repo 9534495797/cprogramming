6e3b8267b2b726e3e98a039600ac037640e13b17
9534495797 <chaubeyankit927@gmail.com>
2023-02-28 19:19:15 +0530

    ok

---
+++ gotostatemment.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+//int main(){
+//	int n=10;
+//A:
+//	printf("%d",n);
+//	n=n-1;
+//	if(n>0)
+//	goto A;
+//	
+//}
+// 
+//int main(){
+//	printf("a%%");
+//}
+
+
+int main(){
+	printf("%5d\n",1);
+	printf("%5d\n",12);
+	printf("%5d\n",123);
+	printf("%5d\n",1234);
+	printf("%5d\n",12345);
+	
+}
+
+
