1ec15d4d08a5bfb036084a230d6ec7d39d7b0d73
9534495797 <chaubeyankit927@gmail.com>
2023-03-01 20:46:56 +0530

    ok

---
+++ factorial.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	int n,i;
+	printf("your input=");
+	scanf("%d",&n)
+     factorial=1;
+	if(x==0){
+		printf("factorial of o is one",n);
+	}
+	else{
+		for(i=1;i<n;i++){
+			printf("factorial is %llu",i);
+			factorial *=i;
+		}
+	}
+}
+
+//int main(){
+//	int n=5,i,mul=1;
+//	for(i=1;i<=5;i=i+1){
+//		mul*=i;
+//
+//	}
+//	printf("%d",mul);
+//	
+//}
