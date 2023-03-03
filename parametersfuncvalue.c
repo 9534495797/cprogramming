24285082631e645459427a02fe9b0db95d6e13d4
9534495797 <chaubeyankit927@gmail.com>
2023-03-03 19:10:20 +0530

    ok

---
+++ parametersfuncvalue.c
@@ -0,0 +1,53 @@
+//parametered function
+// call by value 
+// call by refrence
+// this is called call function by value
+//#include<stdio.h>
+//int sum(int a, int b);
+//int main(){
+//	int a1,b1;
+//	printf("enter two number=");
+//	scanf("%d %d",&a1,&b1);
+//	int c=sum(a1,b1);  //a1 b1 are parameters
+//	printf("%d",c);
+//}
+//int sum(int a,int b){
+//	return a+b;
+//}
+
+
+// factorial question by call by value
+//int fact(int a);
+//int main (){
+//	int a1;
+//	printf("ente ryour value=");
+//	scanf("%d",&a1);
+//	int c=sum(a1);
+//	printf("%d",c);
+//}
+//int sum(int a){
+//	int i,fact=1;
+//	for(i=1;i<=a;i++){
+//		fact=fact*i;
+//	}
+//	return fact;
+//	
+//}
+
+#include<stdio.h>
+int ab(int a);
+int main(){
+	int a1;
+	printf("enter your value=");
+	scanf("%d",&a1);
+	if (a1==1){
+		printf("one");
+	if(a1==3){
+		printf("three")
+	else{
+		break;
+	}
+	}
+	}
+}
+
