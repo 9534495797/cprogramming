5d9775ac4efedb7b03093a2f2cfbbc9ab846f9db
9534495797 <chaubeyankit927@gmail.com>
2023-03-06 20:09:15 +0530

    ok

---
+++ p5.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int x;
+	printf("your value=");
+	scanf("%d",&x);
+	if(x%2==0){
+		printf("even");
+	//else if(x==0){
+		//printf("0");
+	}
+	else{
+		printf("odd");
+	}
+	//
+	}
+	
+	
+
