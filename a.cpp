aabb26af3a2d6c73cec059d9541920b57b96c056
9534495797 <chaubeyankit927@gmail.com>
2023-03-07 19:17:37 +0530

    ok

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//int main(){
+//	char ch;
+//	ch=getchar();//get the input 
+//	putchar(ch);// prtint the input 
+//	char c[10];
+//	gets(c);
+//	puts(c);
+//	
+//}
+int main(){
+	getche();//it do both take input and print
+	
+	
+}
