d5c948a9ceb7c1121183c345201a46da6b328747
9534495797 <chaubeyankit927@gmail.com>
2023-03-07 19:18:25 +0530

    ok

---
+++ loopbook.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+//int main(){
+//	char i=97;
+//	for(; ;){
+//		printf("%c",i++);
+//		if(i==107){
+//			goto stop;
+//		}
+//		//goto stop;
+//	}
+//	stop:;
+//}
+
+
+//int main(){
+//	int x;
+//	for(x=7;x<100;x++){
+//		if (x%4==0 && x%5==4 && x%6==4 ){
+//			printf("%d",x);
+//		}
+//	}
+//}
+
+
+
