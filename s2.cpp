f1e8052822f2ab72dc424e990d415e380be028d3
9534495797 <chaubeyankit927@gmail.com>
2023-03-12 19:27:36 +0530

    ok

---
+++ s2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//int main(){
+//	char c='*';
+//	printf("%c",c);
+//}
+int main(){
+	int x,y;
+	printf("values=");
+	scanf("%d %d",&x,&y);
+	printf("%d %d %d %f %d",x+y,x-y,x*y,x/y,x%y);
+}
