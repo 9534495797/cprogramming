cfb7d8e5782e61a07258ef90ddec290c823357e0
9534495797 <chaubeyankit927@gmail.com>
2023-03-14 17:57:27 +0530

    ok

--- a.cpp
+++ a.cpp
@@ -1,15 +1,4 @@
 #include<stdio.h>
-//int main(){
-//	char ch;
-//	ch=getchar();//get the input 
-//	putchar(ch);// prtint the input 
-//	char c[10];
-//	gets(c);
-//	puts(c);
-//	
-//}
 int main(){
-	getche();//it do both take input and print
-	
-	
+	printf("helloworld");
 }
