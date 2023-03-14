33fec55531182c0a5dc691683b7669ddf6afca43
9534495797 <chaubeyankit927@gmail.com>
2023-03-14 18:00:07 +0530

    ok

---
+++ starpractice.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+//int main(){
+//	int x,i,j;
+//	printf("enter your value=");
+//	scanf("%d",&x);
+//	for(i=1;i<=x;i++){
+//		for(j=1;j<=i;j++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+//int main(){
+//	int x,i,j;
+//	printf("enter your value");
+//	scanf("%d",&x);
+//	for(i=1;i<=x;i++){
+//		for(j=1;j<=i;j++){
+//			printf("%d",i);
+//		}
+//		printf("\n");
+//	}
+//}
+
+int main(){
+	int x,i;
+	for(i=1;i<=x;i++){
+		return i;
+	}
+}
+
+
+
