e6342667462a3b9bda14ff285b1720860a0d5190
9534495797 <chaubeyankit927@gmail.com>
2023-03-17 06:39:08 +0530

    ok

---
+++ t.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//printf("%d %d %d %d",++a,a++,a--,--a);
+//	printf("%d",++a);
+//		printf("%d",--a);
+//			printf("%d",a++);
+//				printf("%d",a--);
+//	}int main(){
+//	int a=0;
+
+int main(){
+	int x=0;
+	printf("%d",++x + x++ + --x + x-- );
+}
+	
