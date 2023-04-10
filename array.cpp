5a0b806bdef057a366b1b2a8b4b305ac150d2971
9534495797 <chaubeyankit927@gmail.com>
2023-04-10 06:25:24 +0530

    ok

---
+++ array.cpp
@@ -0,0 +1,62 @@
+//#include<stdio.h>
+//int main(){
+//	
+//	int a[5]={1,2,3,};
+//	int i;
+//	for(i=0;i<5;i++){
+//		printf("%d \n",a[i]);
+//	}}
+
+//#include<stdio.h>
+//
+//int main(){
+//	int a[5];
+//	int i;
+//	printf("enter the values");
+//		for(i=0;i<5;i++){
+//			scanf("%d",&a[i]);
+//		}
+//	for(i=0;i<5;i++){
+//		printf("%d \n",a[i]);
+//	}
+//}
+
+//array sum
+//#include<stdio.h>
+//int main(){
+//	int a[5];
+//	int i,add=0;
+//	printf("enter the values");
+//		for(i=0;i<5;i++){
+//			scanf("%d",&a[i]);
+//		}
+//	for(i=0;i<5;i++){
+//		add+=a[i];
+//		
+//	}
+//	printf("%d \n",add);
+//}
+
+#include<stdio.h>
+int main(){
+	int i,size,a,b;
+	printf("size of array");
+	for(i=0;i<size;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++){
+		c[i]=a[i]+b[i];
+	}
+	for(i=0;i<size;i++){
+		printf("%d",a[i]);
+	}
+	printf("\n");
+	for(i=0;i<size;i++){
+		printf("%d",b[i]);
+	}
+	printf("\n");
+	for(i=0;i<size;i++){
+		printf("%d",c[i]);
+	}
+	
+}
