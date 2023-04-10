024c1fa6be69570f06e7f86d461452e690c4c2be
9534495797 <chaubeyankit927@gmail.com>
2023-04-10 07:17:03 +0530

    ok

---
+++ array2.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+//int main(){
+//	int a[5]={23,55,15,3,1};
+//	int i;
+//	for(i=0; i<5; i++){
+//	printf("%d\n",a[i]);
+//}
+//}
+//int main(){
+//	int a[4]={1,2,3,};
+//	int i;
+//	for(i=0;i<4;i++){
+//		printf("%d \n",a[i]);
+//	}
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[5];
+//	int i;
+//	printf("enter the numbers");
+//	for(i=0; i<5; i++){
+//		scanf("%d",&a[i]);}
+//		for(i=0; i<5; i++){
+//	printf("%d\n",a[i]);
+//}
+//}
+
+//write a c program addition of two array
+
+//#include<stdio.h>
+//int main()
+//{
+//int a[5];
+//	int i,sum=0;
+//	printf("enter the numbers");
+//	for(i=0; i<5; i++){
+//		scanf("%d",&a[i]);}
+//		for(i=0; i<5; i++){
+//	sum+=a[i];}
+//	printf("%d\n",sum);	
+//}
+
+
+//#include<stdio.h>
+//int main(){
+//	int size;
+//	printf("enter the size of array");
+//	scanf("%d",&size);
+//	int a[size],i;
+//	for(i=0;i<size;i++){
+//		scanf("%d",&a[i]);
+//	}
+//	for (i=0;i<size;i++){
+//		printf("%d",a[i]);
+//	}
+//}
+
+//insertion
+//#include<stdio.h>
+//int main(){
+//	int a[100],size,position,value,i;
+//	printf("enter the size");
+//	scanf("%d",&size);
+//	for(i=0;i<size;i++){
+//		scanf("%d",&a[i]);
+//	}
+//	printf("enter position");
+//	scanf("%d",&position);
+//	printf("enter value");
+//	scanf("%d",&value);
+//	for(i=size-1;i>=position-1;i--){
+//		a[i+1]=a[i];
+//		
+//	}
+//	a[position-1]=value;
+//	for(i=0;i<size+1;i++){
+//		printf("%d\n",a[i]);
+//	}
+//
+//}
+
+
+
+//deletion
+//#include<stdio.h>
+//int main(){
+//	int a[100],n,i,position;
+//	printf("enter the size");
+//	scanf("%d",&n);
+//	for(i=0;i<n;i++){
+//		scanf("%d",&a[i]);
+//	}
+//	printf("enter position");
+//	scanf("%d",&position);
+//	for(i=position-1;i<n;i++){
+//		a[i]=a[i+1];
+//		
+//	}
+//	
+//	for(i=0;i<n-1;i++){
+//		printf("%d\n",a[i]);
+//	}
+//
+//}
+
