38c3437b8678430cbce71c5494156ac3da68d6ba
9534495797 <chaubeyankit927@gmail.com>
2023-04-25 07:03:53 +0530

    ok

---
+++ nestedstructure.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+//struct person{
+//	char name[20];
+//	int age;
+//	struct student{
+//		int reg;
+//		char section[3];
+//	}s;// s written here because to make student local to global
+//};
+//int main(){
+//	struct person p;
+//	//struct student s;  this will not work here
+//	scanf("%s",&p.name);
+//	scanf("%d",&p.s.reg);
+//	printf("%d",p.s.reg);
+//	scanf("%s",&p.s.section);
+//	printf("%s",p.s.section);
+//	
+//}
+
+// also called function in it
+//struct person{
+//	char name[20];
+//	int age;
+//	
+//};
+//void emp1(){
+//	struct emp{
+//		int empid;
+//		float salary;
+//		
+//	}e{123,3456};
+//	printf("%d %f",e.empid,e.salary);
+//}
+//int main(){
+//	struct person p;
+//	scanf("%s",&p.name);
+//	printf("%s\n",p.name);
+//	emp1();
+//	}
