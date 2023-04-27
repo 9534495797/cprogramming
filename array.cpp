b59f79375be7f505ad4efa78fd38c9475dd4a8a5
9534495797 <chaubeyankit927@gmail.com>
2023-04-27 06:39:17 +0530

    ok

--- array.cpp
+++ array.cpp
@@ -22,20 +22,20 @@
 //}
 
 //array sum
-//#include<stdio.h>
-//int main(){
-//	int a[5];
-//	int i,add=0;
-//	printf("enter the values");
-//		for(i=0;i<5;i++){
-//			scanf("%d",&a[i]);
-//		}
-//	for(i=0;i<5;i++){
-//		add+=a[i];
-//		
-//	}
-//	printf("%d \n",add);
-//}
+#include<stdio.h>
+int main(){
+	int a[5];
+	int i,add=0;
+	printf("enter the values");
+		for(i=0;i<5;i++){
+			scanf("%d",&a[i]);
+		}
+	for(i=0;i<5;i++){
+		add+=a[i];
+		
+	}
+	printf("%d \n",add);
+}
 
 #include<stdio.h>
 int main(){
