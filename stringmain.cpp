796341c55a1c7fbfeeb9a40c0d0a229f096ca669
9534495797 <chaubeyankit927@gmail.com>
2023-04-29 07:19:31 +0530

    ok

---
+++ stringmain.cpp
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<string.h>
+//strcpy copy one into anther
+//strcmp cmp to string
+//strcat concatinate
+//int main(){
+//	char ch[]="hello123@";
+//	char ch1[10];  // it is anti char array
+//	strcpy(ch1,ch);
+//	printf("%s",ch1);
+//	
+//} //it will copy whole value
+
+//int main(){
+//	char ch[]="hello123@";
+//	char ch1[10];  // it is anti char array
+//	strncpy(ch1,ch,3);
+//	printf("%s",ch1);
+//	
+//}// it will copy first three only
+
+
+//cancatinate  1st will cancatinate with 2nd
+//int main(){
+//	char ch[]="hello123@";
+//	char ch1[10];  // it is anti char array
+//	strncpy(ch1,ch,3);
+//	printf("%s\n",ch1);
+//	printf("%s",strcat(ch1,ch));
+//	
+//}
+
+//int main(){
+//	char ch[]="hello123@";
+//	char ch1[10];  // it is anti char array
+//	strncpy(ch1,ch,3);
+//	printf("%s\n",ch1);
+//	printf("%s",strncat(ch1,ch,2));// it will reduce from 2nd
+//	
+//}//ctn with desired no. of char
+
+
+//int main(){
+//	char ch[]="hello123@";
+//	char ch1[10];  // it is anti char array
+//	strcpy(ch1,ch);//here it is end of array
+//	printf("%s\n",ch1);
+//	strncpy(ch1,ch,3);
+//	printf("%s",ch1);//it will print same value b/c itis end of array there
+//	
+//}
+
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[12];
+//	printf("%s\n",strcpy(ch1,ch));
+//	printf("%s",strncpy(ch1,ch,2));// it will not replace because it is copy not concTINATION
+//	
+//}
+
+// compare string
+//
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[12];
+//	printf("%s\n",strncpy(ch1,ch,2));
+//	printf("%d\n",strcmp(ch1,ch));
+//	printf("%d\n",strcmp(ch,ch1));
+//	printf("%s\n",strcpy(ch1,ch));
+//	printf("%d\n",strcmp(ch,ch1));
+//	
+//// it will not replace because it is copy not concTINATION
+//	
+//}
+
+
+//int main(){
+//	char ch[]="abcd";
+//	char ch1[]="wert"
+//	//printf("%s\n",strncpy(ch1,ch,2));
+//	printf("%d\n",strcmp(ch1,ch));
+//	printf("%d\n",strcmp(ch,ch1));
+//	printf("%s\n",strcpy(ch1,ch));
+//	printf("%d\n",strcmp(ch,ch1));
+//	
+//// it will not replace because it is copy not concTINATION
+//	
+//}
+
+
+
+
+//question
+//int main(){
+//	char x='a'+1;
+//	printf("%c\n",x);
+//	printf("%c\n",++x);
+//}
+
+//int main(){
+//	char x='a'+'c';
+//	printf("%c\n",x);
+//	//printf("%c\n",++x);
+//}//here output will be -
+
+
+//int main(){
+//	char x='c'-1;
+//	printf("%c\n",x);
+//	//printf("%c\n",++x);
+//}
+
+
+
+
