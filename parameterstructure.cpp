87f86d21615914ca6f42b43ff2c8f6bb47c1caf6
9534495797 <chaubeyankit927@gmail.com>
2023-05-03 08:01:21 +0530

    ok

---
+++ parameterstructure.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+//struct emp{
+//	char name[10];
+//	int empid;
+//	float salary;
+//};
+//void display(struct emp e){
+//	printf("%s %d %f",e.name,e.empid,e.salary);
+//}
+//int main(){
+//	struct emp e1;
+//	scanf("%s",&e1.name);
+//	scanf("%d",&e1.empid);
+//	scanf("%f",&e1.salary);
+//	display(e1);
+//}
+
+
+//crete a structure number which includes 3 members 2 int and 1 float,create a parametrize func which display the addition of two in and float values
+//struct number{
+//	int x;
+//	int y;
+//	float z;
+//	
+//	
+//};
+//void display(struct number n){
+//	printf("%d  %f",n.x+n.y,n.z);
+//	
+//}
+//int main(){
+//	struct number n1;
+//	scanf("%d",&n1.x);
+//	scanf("%d",&n1.y);
+//	scanf("%f",&n1.z);
+//	
+//	display(n1);
+//}
+
+
+
+//struct car{
+//	char name[20];
+//	int year;
+//	float price;
+//	
+//}c1,*c2;
+//int main(){
+//	c2=&c1;// value of c1 is assining to c2
+//	printf("enter values= ");
+//	scanf("%s",&c2->name);
+//	scanf("%d",&c2->year);
+//	scanf("%f",&c2->price);
+//	printf("%s %d %f\n",c2->name,c2->year,c2->price);
+//}
+//struct sum{
+//	int x;
+//	int y;
+//	float z;
+//	
+//}s,*s1;
+//
+//int main(){
+//	s1=&s;
+//	
+//	scanf("%d",&s1->x);
+//	scanf("%d",&s1->y);
+//	scanf("%f",&s1->z);
+//	printf("%d %f",s1->x+s1->y,s1->z);
+//}
