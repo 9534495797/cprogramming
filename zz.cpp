98ca654d43aff5a6f73f2811c15f8ff2ee796b9f
9534495797 <chaubeyankit927@gmail.com>
2023-05-06 05:57:40 +0530

    ok

---
+++ zz.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+//int main(){
+//	printf("hello world\n");
+//	int x,y;
+//	printf("enter ur values = \n");
+//	scanf("%d",&x);
+//	scanf("%d",&y);
+//	printf("sum=%d \n",x+y);
+//	printf("diff=%d \n",x-y);
+//	printf("mul=%d \n",x*y);
+//	printf("div=%d \n",x/y);
+//	
+//	
+//}
+
+//struct student{
+//	char name;
+//	int roll;
+//	float marks;
+//}s;
+//int main(){
+//	//s.name='ankit';
+//	s.roll=16;
+//	s.marks=600;
+//	printf("%S\n %d\n %f\n",s.name,s.roll,s.marks);
+//}
