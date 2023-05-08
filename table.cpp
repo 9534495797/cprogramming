282e8c5f2e77857c752512c3d0e560734bc1900e
9534495797 <chaubeyankit927@gmail.com>
2023-05-08 06:27:35 +0530

    ok

---
+++ table.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int num,num1,i,j;
+	printf("enter num =");
+	scanf("%d",&num);
+	scanf("%d",&num1);
+	for(i=1;i<=10;i++){
+		printf("%d\n",num*i);
+		
+	}
+	for(j=1;j<=10;j++){
+		printf("%d\n",num1*j);
+	}
+
+}
