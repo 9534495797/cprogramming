0d85a66cba7eb683c58387674cb36e609be804a5
9534495797 <chaubeyankit927@gmail.com>
2023-05-09 06:37:38 +0530

    ok

---
+++ strpr.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//struct student{
+//	char name[40];
+//	int roll;
+//	float marks;
+//}s;
+//int main(){
+//	s.name='ankit';
+//	s.roll=16;
+//	s.marks=600;
+//	printf("%s %d %f",s.name,s.roll,s.marks);
+//}
+
+int main(){
+	int n,i;
+	printf("n--");
+	scanf("%d",&n);
+	for(i=1;i<=10;i++){
+		printf("%d \n",n*i);
+	}
+}
