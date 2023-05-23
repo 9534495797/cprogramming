4c18d69611eac10b51ecf7868a653cab9ec32887
9534495797 <chaubeyankit927@gmail.com>
2023-05-23 16:15:43 +0530

    ok

---
+++ dcbds.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+//int main(){
+//	int i=10;
+////	void *p=&i;
+////	printf("%f",*(int*)p);
+////	return  0;
+//printf("%f",i);
+//}
+
+
+
+//int main(){
+//	union var{
+//		int a,b;
+//		
+//	};
+//	union var v;
+//	v.a=10;
+//	v.b=20;
+//	printf("%d",v.a);
+//
+//	return 0;
+//}
+
+int main(){
+	char *str="hello, woeld\n";
+	printf("%d",strlen(str));
+}
