3b0f81a710c09c6c0e4c3c4fac5dd9044a1417d3
9534495797 <chaubeyankit927@gmail.com>
2023-05-23 16:14:31 +0530

    ok

--- array.cpp
+++ array.cpp
@@ -8,7 +8,6 @@
 //	}}
 
 //#include<stdio.h>
-//
 //int main(){
 //	int a[5];
 //	int i;
@@ -22,41 +21,66 @@
 //}
 
 //array sum
-#include<stdio.h>
-int main(){
-	int a[5];
-	int i,add=0;
-	printf("enter the values");
-		for(i=0;i<5;i++){
-			scanf("%d",&a[i]);
-		}
-	for(i=0;i<5;i++){
-		add+=a[i];
-		
-	}
-	printf("%d \n",add);
-}
+//#include<stdio.h>
+//int main(){
+//	int a[5];
+//	int i,add=0;
+//	printf("enter the values");
+//		for(i=0;i<5;i++){
+//			scanf("%d",&a[i]);
+//		}
+//	for(i=0;i<5;i++){
+//		add+=a[i];
+//		
+//	}
+//	printf("%d \n",add);
+//}
 
 #include<stdio.h>
+
+
 int main(){
-	int i,size,a,b;
-	printf("size of array");
-	for(i=0;i<size;i++){
-		scanf("%d",&a[i]);
+	int x,sum=0,i;
+	printf("enter x\n");
+	scanf("%d",&x);
+	for(i=0;i<=x;i++){
+		sum+=i;
 	}
-	for(i=0;i<size;i++){
-		c[i]=a[i]+b[i];
-	}
-	for(i=0;i<size;i++){
-		printf("%d",a[i]);
-	}
-	printf("\n");
-	for(i=0;i<size;i++){
-		printf("%d",b[i]);
-	}
-	printf("\n");
-	for(i=0;i<size;i++){
-		printf("%d",c[i]);
-	}
-	
+	printf("%d",sum);
 }
+//int main(){
+//	int a[5];
+//	int i ,mul=1;
+//	printf("enter values=");
+//	for(i=0;i<5;i++){
+//		scanf("%d",&a[i]);
+//	}
+//	for(i=0;i<5;i++){
+//		mul*=a[i];
+//	}
+//	printf("%d",mul);
+//}
+
+//#include<stdio.h>
+//int main(){
+//	int i,size,a,b;
+//	printf("size of array");
+//	for(i=0;i<size;i++){
+//		scanf("%d",&a[i]);
+//	}
+//	for(i=0;i<size;i++){
+//		c[i]=a[i]+b[i];
+//	}
+//	for(i=0;i<size;i++){
+//		printf("%d",a[i]);
+//	}
+//	printf("\n");
+//	for(i=0;i<size;i++){
+//		printf("%d",b[i]);
+//	}
+//	printf("\n");
+//	for(i=0;i<size;i++){
+//		printf("%d",c[i]);
+//	}
+//	
+//}
