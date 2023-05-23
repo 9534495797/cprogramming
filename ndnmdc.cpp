ea98548cb7ee347ff2935868e12f16d8e9d9ddbc
9534495797 <chaubeyankit927@gmail.com>
2023-05-23 16:16:12 +0530

    ok

---
+++ ndnmdc.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//int main(){
+//	int a=10,b=5,c=5;
+//	int d=b+c==a;
+//	printf("%d",d);
+//}
+//int main(){
+//	int x=1,z=3;
+//	int y=x<<z;
+//	printf("%d",y);
+//}
+//
+//int main(){
+//	int x=(int)3.9 +1.2;
+//	printf("%d",x);
+//}
+
+int main(){
+	printf("%d ",'A'+'B');
+}
