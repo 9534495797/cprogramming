63fbde0a010a31814583af01ff627bfef494c4a5
9534495797 <chaubeyankit927@gmail.com>
2023-05-27 20:26:07 +0530

    ok

---
+++ stringall.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+//int main(){
+//
+//	char name[5]={'a','n','k','i','t'};
+//	printf("%s\n",name);
+//	printf("%.3s\n",name);
+//	printf("%.6s",name);
+//	
+//}
+
+//int main(){
+//
+//	char name1[5]={'a','n','k','i','t'};
+//	char name2[6]={'a','n','k','i','t'};
+//	printf("%s\n",name1);
+//	printf("%s",name2);
+//	
+//}
+
+
+
+//int main(){
+//	char x1[8]="welcome";
+//	char x2[8]={'w','e','l','c','o','m','e'};
+//	char x3[8]={{'w'},{'e'},{'l'},{'c'},{'o'},{'m'},{'e'}};
+//	printf("%s",x1);
+//	printf("%s",x2);
+//	printf("%s",x3);
+//}
+
+int main(){
+	char text[]="have a nice day";
+	int i=0;
+	while(i<15){
+		printf("%c",text[i]);
+		i++;
+	}
+}
