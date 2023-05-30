90c0d3fefa83226e955a33f60e7a052fc546bfbc
9534495797 <chaubeyankit927@gmail.com>
2023-05-30 19:30:50 +0530

    ok

---
+++ withoutcolon.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//int main()
+//{
+//    if (printf("Hello World!\n")) {}
+//    return 0;
+//}
+
+
+//int main(){
+//	printf("hello world");
+//}
+
+int main(){
+	if(printf("hello world")){
+	}
+}
