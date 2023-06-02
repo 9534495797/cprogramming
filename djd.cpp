73b5f9e12346cbbe70ce7d0010d0bb329549f715
9534495797 <chaubeyankit927@gmail.com>
2023-06-02 06:11:00 +0530

    ok

---
+++ djd.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+struct student{
+	char name[10];
+	int age;
+}s;
+int main(){
+
+	printf("enter your values=");
+	scanf("%c",&name);
+	scanf("%d",&age);
+	printf("%c",s.name);
+	printf("%d",s.age);
+}
